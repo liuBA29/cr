b5c3273ff18eab067bc7e28858a6ba9c9dbf8fc4
liuBA <lu-@rambler.ru>
2023-10-25 14:08:47 +0300

    second commit

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from crm_app.views import *
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('crm_app/', index),
 ]
